f082ed8a7406121db0ad92b898203b78cccafa7c
Nikita Ushakov <nikita.ushakov0000@outlook.com>
2025-03-13 21:48:31 +0000

    chore: release 1.0.1 Release-As: 1.0.1

--- cmake/menusystem/generate_pplugin.cmake
+++ cmake/menusystem/generate_pplugin.cmake
@@ -2,6 +2,12 @@
 # Copyright (C) 2025 Wend4r
 # Licensed under the MIT license. See LICENSE file in the project root for details.
 
+file(READ "${CMAKE_SOURCE_DIR}/version.txt" VERSION_FILE_CONTENTS)
+string(STRIP "${VERSION_FILE_CONTENTS}" VERSION_FILE_CONTENTS)
+set(S2MENU_VERSION "${VERSION_FILE_CONTENTS}" CACHE STRING "Set version name")
+set(S2MENU_PACKAGE "s2menu" CACHE STRING "Set package name")
+string(REPLACE "v" "" S2MENU_VERSION "${S2MENU_VERSION}")
+
 configure_file(
 	${CMAKE_SOURCE_DIR}/s2menu.pplugin.in
 	${CMAKE_BINARY_DIR}/s2menu.pplugin
